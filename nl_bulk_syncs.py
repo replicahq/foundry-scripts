e6485a2c499bea1ee8124c285ec9a533e41e2d32
Amy Wang <amyewang115@gmail.com>
2023-11-27 14:16:32 -0500

    add nl lists

--- nl_bulk_syncs.py
+++ nl_bulk_syncs.py
@@ -11,7 +11,7 @@ PARENT_COMPASS_FOLDER_RID = "ri.compass.main.folder.703b31db-4acd-4c2d-807c-3682
 MARKINGS = []
 
 REGIONS = [
-    # "alaska",
+    "alaska",
     "cal_nev",
     "great_lakes",
     "hawaii",
@@ -26,8 +26,8 @@ REGIONS = [
 ]
 # DAYS = ["thursday", "saturday"]
 # SEASONS = ["2019_Q4", "2021_Q2", "2021_Q4", "2022_Q4", "2023_Q2"]
-DAYS = ["thurs"]
-SEASONS = ["2023_q2"]
+DAYS = ["saturday"]
+SEASONS = ["2023_Q2"]
 
 
 def generate_table_names():
@@ -36,9 +36,7 @@ def generate_table_names():
         for season in SEASONS:
             for day in DAYS:
                 tables.append(
-                    "core-data-service-prod.nl_breakdown.{}_{}_{}".format(
-                        region, season, day
-                    )
+                    "model-159019.nl_breakdown.{}_{}_{}".format(region, season, day)
                 )
     return tables
 
@@ -65,6 +63,9 @@ def create_dataset(table):
     )
     if response.status_code in [200, 204]:
         print("SUCCESS: Created dataset: {}".format(response.json()["rid"]))
+        f = open("nl/nl_dataset_rid.txt", "a")
+        f.write(f"\n{response.json()['rid']},")
+        f.close()
     else:
         print("FAILED TO CREATE DATASET")
     return response.json()
@@ -121,7 +122,10 @@ def create_sync(dataset_rid, project, folder, table):
         json=json_data,
     )
     if response.status_code in [200, 204]:
-        print("SUCCESS: Created Sync")
+        print("SUCCESS: Created Sync: {}".format(response.json()))
+        f = open("nl/nl_sync_rid.txt", "a")
+        f.write(f"\n{response.json()},")
+        f.close()
     else:
         print("FAILED TO CREATE SYNC")
     pass
@@ -131,7 +135,6 @@ def main():
     tables = generate_table_names()
     print(tables)
     for location in tables:
-        # print("hello")
         project, folder, table = location.split(".")
         print("\n\n TABLE: {}".format(location))
         dataset = create_dataset(table)
